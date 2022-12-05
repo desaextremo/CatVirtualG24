4dd7461b1f02026f1f427b9bc56b7f283d59f2e3
Crhistian Ovalle <covalle@desarrolloextemo.com.co>
2022-12-04 20:07:37 -0500

    Delete CatVirtualG24 directory

--- CatVirtualG24/src/main/java/com/unab/catvirtual/catvirtualg24/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unab.catvirtual.catvirtualg24.repository;
-
-import com.unab.catvirtual.catvirtualg24.enitity.Book;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookRepository extends MongoRepository<Book,String> {
-}
